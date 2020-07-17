38c4cffacb36ade87f7f55ed5024020ebe67ffd7
vania-sgn <Skladchikov.vania@gmail.com>
2020-07-17 14:32:31 +0300

    now

--- src/Cycles/For_Cycles.java
+++ src/Cycles/For_Cycles.java
@@ -3,8 +3,12 @@ package Cycles;
 public class For_Cycles {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
-        for (int i = 100; i > 10; i -= 2) {
+        for (int i = 100; i > 10; ) {
+
+
+            i -= 2;
             System.out.println(i);
+            break;
 
         }
     }
