751dc9f259b1b183e5f9c57397506ab2bf319036
vania-sgn <Skladchikov.vania@gmail.com>
2020-07-17 13:08:18 +0300

    now

---
+++ src/Cycles/do_while_Cycles.java
@@ -0,0 +1,16 @@
+package Cycles;
+
+public class do_while_Cycles {
+    public static void main(String[] args) {
+        int x = 0;
+
+        do {
+            System.out.println(x);
+        }
+        while (x < 0);
+    }
+}
+
+
+
+
