2275c1fb5f0621501bf83819acdb7807ef13b656
vania-sgn <Skladchikov.vania@gmail.com>
2020-07-17 12:51:09 +0300

    the end

--- src/Cycles.java
+++
@@ -1,5 +0,0 @@
-public class Cycles {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/Cycles/While_Cycles.java
@@ -0,0 +1,11 @@
+package Cycles;
+
+public class While_Cycles {
+    public static void main(String[] args) {
+        int j=0;
+        while(j<20){
+            System.out.println(j);
+            j++;
+        }
+    }
+}
