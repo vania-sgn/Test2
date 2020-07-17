ad251920a001c909a584cc32f41bb6d602e1dbb2
vania-sgn <Skladchikov.vania@gmail.com>
2020-07-17 18:09:50 +0300

    Arrays

---
+++ src/Arrays/Multidimensional_array.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class Multidimensional_array {
+    public static void main(String[] args) {
+        char[][] array= new char[3][3];
+        array [0][0]= 'c';
+        System.out.println(array [0][0]);
+
+
+    }
+}
