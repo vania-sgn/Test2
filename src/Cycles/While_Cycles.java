b874de0b4b2c62049d0892f691eea59d702b756b
vania-sgn <Skladchikov.vania@gmail.com>
2020-07-17 12:59:35 +0300

    now

--- src/Cycles/While_Cycles.java
+++ src/Cycles/While_Cycles.java
@@ -2,10 +2,16 @@ package Cycles;
 
 public class While_Cycles {
     public static void main(String[] args) {
-        int j=0;
+        /*int j=0;
         while(j<20){
             System.out.println(j);
-            j++;
+            j++;*/
+        for (int i = 0; i < 2; i++) {
+            boolean isTrue = true;
+            while (isTrue) {
+                System.out.println("111");
+                isTrue = false;
+            }
 
         }
     }
