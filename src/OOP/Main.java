104cda6c34ce2a7fb0d675ba955b7d57afab7770
vania-sgn <Skladchikov.vania@gmail.com>
2020-07-31 12:58:59 +0300

    OOP

---
+++ src/OOP/Main.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Person Vitya= new Person();
+        System.out.println(Vitya.height);
+
+    }
+}
---
+++ src/OOP/Person.java
@@ -0,0 +1,7 @@
+package OOP;
+
+public class  Person {
+        public int height =180;
+//        System.out.println("1234");
+    }
+
