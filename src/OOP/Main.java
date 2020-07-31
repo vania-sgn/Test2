17981602098af0818cbddf5bb77d087d10f44b43
vania-sgn <Skladchikov.vania@gmail.com>
2020-07-31 13:28:31 +0300

    OOP

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -2,8 +2,10 @@ package OOP;
 
 public class Main {
     public static void main(String[] args) {
-        Person Vitya = new Person();
-        System.out.println(Vitya.height);
+        Person Vitya = new Person(12,"Vitya");
+        System.out.println(Vitya.name+ "`s height is "+ Vitya.height);
         Vitya.say("Georg");
+        Person Vlad = new Person();
+        System.out.println(Vlad.height);
     }
 }
--- src/OOP/Person.java
+++ src/OOP/Person.java
@@ -2,10 +2,21 @@ package OOP;
 
 public class Person {
     public int height = 180;
+    public String name="Default";
+
+    public Person() {
+
+    }
 
     //        System.out.println("1234");
     public void say(String name) {
         System.out.println("Hello," + name);
+    }
+
+    public Person(int h, String n) {
+        height = h;
+        name=n;
+
     }
 }
 
